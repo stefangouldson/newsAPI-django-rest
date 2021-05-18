9c1d17493a18197f8d3d27f2052143340c5f3c0b
Stefan Gouldson <sgouldson@CASPIAN71.caspian.local>
2021-05-18 14:42:52 +0100

    initial commit

---
+++ news/api/serializers.py
@@ -0,0 +1,77 @@
+from rest_framework import serializers
+from news.models import Article, Journalist
+from datetime import datetime
+from django.utils.timesince import timesince
+
+class ArticleSerializer(serializers.ModelSerializer):
+
+    time_since_publication = serializers.SerializerMethodField()
+    # author = JournalistSerializer()
+    # author = serializers.StringRelatedField()
+
+    class Meta:
+        model = Article
+        exclude = ("id",)
+
+    def get_time_since_publication(self, object):
+        publication_date = object.publication_date
+        now = datetime.now()
+        time_delta = timesince(publication_date, now)
+        return time_delta
+    
+    def validate(self, data):
+        """ check that description and title are different """
+        if data["title"] == data["description"]:
+            raise serializers.ValidationError("Title and Description must be different from each other")
+        return data
+
+    def validate_title(self, value):
+        if len(value) < 30:
+            raise serializers.ValidationError("Title must be 30 character long")
+        return value
+
+class JournalistSerializer(serializers.ModelSerializer):
+    articles = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name="article-detail")
+    # articles = ArticleSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = Journalist
+        fields = "__all__"
+
+# class ArticleSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only = True)
+#     author = serializers.CharField()
+#     title = serializers.CharField()
+#     description = serializers.CharField()
+#     body = serializers.CharField()
+#     location = serializers.CharField()
+#     publication_date = serializers.DateField()
+#     active = serializers.BooleanField()
+#     created_at = serializers.DateTimeField(read_only = True)
+#     updated_at = serializers.DateTimeField(read_only = True)
+
+#     def create(self, validated_data): 
+#         print(validated_data)
+#         return Article.objects.create(**validated_data)
+
+#     def update(self, instance, validated_data):
+#         instance.author = validated_data.get('author', instance.author)
+#         instance.title = validated_data.get('title', instance.title)
+#         instance.description = validated_data.get('description', instance.description)
+#         instance.body = validated_data.get('body', instance.body)
+#         instance.location = validated_data.get('location', instance.location)
+#         instance.publication_date = validated_data.get('publication_date', instance.publication_date)
+#         instance.active = validated_data.get('active', instance.active)
+#         instance.save()
+#         return instance
+    
+#     def validate(self, data):
+#         """ check that description and title are different """
+#         if data["title"] == data["description"]:
+#             raise serializers.ValidationError("Title and Description must be different from each other")
+#         return data
+
+#     def validate_title(self, value):
+#         if len(value) < 60:
+#             raise serializers.ValidationError("Title must be 60 character long")
+#         return value
---
+++ news/api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from news.api.views import (ArticleListCreateAPIView, ArticleDetailAPIView, JournalistListCreate)
+# from news.api.views import (article_list_create_api_view, article_detail_api_view)
+
+urlpatterns = [
+    path("articles/", ArticleListCreateAPIView.as_view(), name="article-list"),
+    path("articles/<int:pk>", ArticleDetailAPIView.as_view(), name="article-detail"),
+    path("journalist/", JournalistListCreate.as_view(), name="journalist-list")
+
+    # Function based views
+    # path("articles/", article_list_create_api_view, name="article-list"),
+    # path("articles/<int:pk>", article_detail_api_view, name="article-detail")
+]
